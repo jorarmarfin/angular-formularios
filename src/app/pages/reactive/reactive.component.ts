7e8f4a1cac9c063b5b1c692b08e0ab697c744e59
Luis Mayta <luis.mayta@gmail.com>
2020-08-20 18:04:53 -0500

    formularios 4

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { ValidadoresService } from 'src/app/services/validadores.service';
 
 @Component({
   selector: 'app-reactive',
@@ -9,7 +10,7 @@ import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 })
 export class ReactiveComponent implements OnInit {
   forma:FormGroup;
-  constructor(private fb:FormBuilder) { 
+  constructor(private fb:FormBuilder,private validadores:ValidadoresService) { 
     this.crearFormulario(); 
     this.cargarDataAllFormulario();
   }
@@ -34,10 +35,18 @@ export class ReactiveComponent implements OnInit {
   get pasatiempos(){
     return this.forma.get('pasatiempos') as FormArray;
   }
+  get pass1NoValido(){
+    return this.forma.get('pass1').invalid && this.forma.get('pass1').touched;
+  }
+  get pass2NoValido(){
+      const pass1 = this.forma.get('pass1').value;
+      const pass2 = this.forma.get('pass2').value;
+      return (pass1 === pass2) ? false:true;
+  }
   crearFormulario(){
     this.forma = this.fb.group({
       nombre:['',[Validators.required,Validators.minLength(5)]],
-      apellido:['',[Validators.required,Validators.minLength(5)]],
+      apellido:['',[Validators.required,Validators.minLength(5),this.validadores.noHerrera]],
       correo:['',[Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
       direccion:this.fb.group({
         distrito:['',Validators.required],
@@ -45,6 +54,10 @@ export class ReactiveComponent implements OnInit {
       }),
       pasatiempos:this.fb.array([
       ]),
+      pass1:['',[Validators.required]],
+      pass2:['',[Validators.required]],
+    },{
+      validators:[ this.validadores.passwordsIguales('pass1','pass2')]
     });
   }
   cargarDataAllFormulario(){
@@ -81,5 +94,11 @@ export class ReactiveComponent implements OnInit {
   borrarPasatiempo(i:number){
     this.pasatiempos.removeAt(i);
   }
+
+
+
+
+
+
 
 }
---
+++ src/app/services/validadores.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidadoresService {
+
+  constructor() { }
+  noHerrera(control:FormControl):{[s:string]:boolean}{
+    // el signo ? hace que sea condicional su existencia
+    if (control.value?.toLowerCase() === 'mayta') {
+
+      return {
+        noHerrera:true
+      }
+    }
+    return null;
+
+  }
+  passwordsIguales(pass1Name:string,pass2Name:string){
+    return (formGroup:FormGroup)=>{
+      const pass1Control = formGroup.controls[pass1Name];
+      const pass2Control = formGroup.controls[pass2Name];
+      if (pass1Control.value === pass2Control) {
+        pass2Control.setErrors(null);
+      }else{
+        pass2Control.setErrors({noEsIgual:true});
+      }
+
+    }
+  }
+
+
+
+
+
+
+
+}
